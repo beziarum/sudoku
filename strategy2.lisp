f3c11f3516c30ab3bfbb6798d5671dfdec8cbfda
paul <paul.beziau@laposte.net>
2015-12-08 09:52:25 +0100

    mis au point du mélange des strat exclusives et inclusives, reste à rajouter la gestion des carrées pour la méthode inclusive, mais ça doit pouvoir déjà marcher pour certains cas simples

--- strategy2.lisp
+++ strategy2.lisp
@@ -17,20 +17,51 @@
 
 (defun inclusive-colone (grid i j)
   (let ((possible (aref grid i j)))
-    (loop for j below 9
-       do (setf possible (set-difference possible (aref grid i j))))
+    (loop for y below 9
+       do (if (/= j y)
+	      (setf possible (set-difference possible (aref grid i y)))))
+    ;(print possible)
     (if (null (cdr possible))
 	(car possible)
 	nil)))
 
 (defun inclusive-ligne (grid i j)
   (let ((possible (aref grid i j)))
-    (loop for i below 9
-	 do (setf possible (set-difference possible (aref grid i j))))
+    (loop for x below 9
+       do (if (/= x i)
+	      (setf possible (set-difference possible (aref grid x j)))))
     (if (null (cdr possible))
 	(car possible)
 	nil)))
-  
+
+(defun exclusive-strat (grid)
+    (let ((ret nil))
+      (loop
+	 for i below 9 always (null ret)
+	 do (loop
+	       for j below 9 always (null ret)
+	       do (if (and (null (cdr (aref grid i j)))
+			   (not (null (car (aref grid i j)))))
+		      (progn (setf ret (list j
+					     i
+					    (car (aref grid i j))))
+			     (supprimer-colone grid ret j)
+			     (supprimer-ligne grid ret i)
+			     (supprimer-carre grid ret i j)
+			     (setf (aref grid i j) nil)))))
+      (values-list ret)))
+
+(defun inclusive-strat (grid)
+    (let ((ret nil))
+      (loop for i below 9 always (null ret)
+	 do (loop for j below 9 always (null ret)
+	       do (progn (setf ret
+			       (multiple-value-list (inclusive-colone grid i j))))
+			 (if (null ret)
+			     (setf ret
+				   (multiple-value-list (inclusive-ligne grid i j))))))
+      (values-list ret)))
+
 (let ((pgrid nil)
       (travail-grid nil))
   (defun initialise-tab-strat (grid)
@@ -45,39 +76,20 @@
 		      (supprimer-carre pgrid (aref grid i j) i j)
 		      (setf (aref pgrid i j) nil))))))
 
-  (defun inclusive-standalone ()
-    (let ((ret nil))
-      (loop for i below 9 always (null ret)
-	 do (loop for j below 9 always (null ret)
-	       do (setf ret (inclusive-colone pgrid i j))))
-      ret))
-		   
-
-			 
+  (defun test ()
+    (inclusive-strat pgrid))
   (defun main-standalone ()
-    (let ((ret nil))
-      (loop
-	 for i below 9 always (null ret)
-	 do (loop
-	       for j below 9 always (null ret)
-	       do (if (and (null (cdr (aref pgrid i j)))
-			   (not (null (car (aref pgrid i j)))))
-		      (progn (setf ret (list j
-					     i
-					    (car (aref pgrid i j))))
-			     (supprimer-colone pgrid ret j)
-			     (supprimer-ligne pgrid ret i)
-			     (supprimer-carre pgrid ret i j)
-			     (setf (aref pgrid i j) nil)))))
-      (values (car ret) (cadr ret) (caddr ret)))))
+    (multiple-value-bind (i j v) (exclusive-strat pgrid)
+      (if (not (null v))
+	  (progn (print pgrid) (values i j v))
+	  (inclusive-strat pgrid)))))
 
-
-;; (defun test-strat (init main grid)
-;;   (funcall init grid)
-;;   (labels ((intern-test-strat ()
-;; 	     (multiple-value-bind (i j v) (time (funcall main)
-;; 	       (if (not (null v))
-;; 		   (progn (setf (aref grid j i) v)
-;; 			  (afficher-sudoku grid)
-;; 			  (intern-test-strat))))))
-;;     (intern-test-strat)))
+(defun test-strat (init main grid)
+  (funcall init grid)
+  (labels ((intern-test-strat ()
+	     (multiple-value-bind (i j v) (time (funcall main))
+	       (if (not (null v))
+		   (progn (setf (aref grid j i) v)
+			  (afficher-sudoku grid)
+			  (intern-test-strat))))))
+    (intern-test-strat)))
