001195b9083e4116191a4e33ff28ecddce21fcd4
paul <paul.beziau@laposte.net>
2015-12-09 22:00:40 +0100

    ajout de la stratégie des paires

--- strategy2.lisp
+++ strategy2.lisp
@@ -66,6 +66,70 @@
 			     (supprimer-carre grid (caddr ret) i j)
 			     (setf (aref grid i j) nil)))))
       (values-list ret)))
+
+(defun simplifier-colonne (grid i) 
+  (let ((simp nil))
+    (loop for y1 below 9
+       do (loop for y2 from (1+ y1) below 9
+	     if (and (equal (aref grid i y1) (aref grid i y2))
+		     (= (length (aref grid i y1)) 2))
+	     do (loop for y3 below 9
+		   if (and (/= y1 y2 y3)
+			   (intersection (aref grid i y3)
+					 (aref grid i y2)))
+		   do (progn (setf (aref grid i y3)
+				   (set-difference (aref grid i y3)
+						   (aref grid i y2)))
+			     (setf simp t)
+			     (print (aref grid i y2))))))
+    simp))
+
+(defun simplifier-ligne (grid j)
+  (let ((simp nil))
+    (loop for x1 below 9
+       do (loop for x2 from (1+ x1) below 9
+	     if (and (equal (aref grid x1 j) (aref grid x2 j))
+		     (= (length (aref grid x1 j)) 2))
+	     do (loop for x3 below 9
+		   if (and (/= x1 x2 x3)
+			   (intersection (aref grid x3 j)
+					 (aref grid x2 j)))
+		   do (progn (setf (aref grid x3 j)
+				   (set-difference (aref grid x3 j)
+						   (aref grid x2 j)))
+			     (setf simp t)
+			     (print (aref grid x2 j))))))
+    simp))
+;; on lui donne le coins supérieur gauche d'un carre
+(defun simplifier-carre (grid i j)
+  (let ((simp nil))
+    (loop for x1 from i below (+ 3 i)
+       do (loop for y1 from j below (+ 3 j)
+	     do (loop for x2 from x1 below (+ 3 i)
+		   do (loop for y2 from (if (= x1 x2) (+ 1 y1) 0) below (+ 3 j)
+			 if (equal (aref grid x1 y1) (aref grid x2 y2))
+			 do (loop for x3 from i below (+ 3 i)
+			       do (loop for y3 from i below (+ 3 j)
+				     if (and (or (/= x1 x2 x3) (/= y1 y2 y3))
+					     (intersection (aref grid x3 y3)
+							   (aref grid x2 y2)))
+				     do (progn (setf (aref grid x3 y3)
+						     (set-difference (aref grid x3 y3)
+								     (aref grid x2 y2)))
+					       (setf simp t))))))))
+    simp))
+
+(defun simplifier-sudoku (grid)
+  ;; nil)
+  (let ((simp nil))
+    (loop for n below 9
+       do (progn (setf simp (or (simplifier-colonne grid n) simp)) ))
+	;;	 (setf simp (or (simplifier-colone grid n) simp)) ))
+  		 ;; (if (= (mod n 3) 0)
+  		 ;;     (loop for m below 9 by 3
+  		 ;;	do (setf simp (or (simplifier-carre grid n m) simp))))))
+   simp))
+	    
 ;; (defun pifo-strat (grid)
 ;;   (let ((ret nil))
 ;;     (loop for i below 9 aways (null
@@ -114,10 +178,13 @@
     (inclusive-strat pgrid))
   (defun main-standalone ()
     (print pgrid)
-    (multiple-value-bind (i j v) (exclusive-strat pgrid)
-      (if (not (null v))
-	  (progn (print v) (values i j v))
-	  (progn (print 'caca) (inclusive-strat pgrid))))))
+    (let ((l (multiple-value-list (exclusive-strat pgrid))))
+      (if (null (car l))
+	  (setf l (exclusive-strat pgrid)))
+      (if (and (null (car l))
+	       (simplifier-sudoku pgrid))
+	  (main-standalone)
+	  (values-list l)))))
 
 (defun test-strat (init main grid)
   (funcall init grid)
