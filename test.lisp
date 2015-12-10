9cafa34cf203da5884f53b235879f8fba5aa6c0a
Raitei3 <antoine24.borde@gmail.com>
2015-12-10 11:34:21 +0100

    on continue les test

--- test.lisp
+++ test.lisp
@@ -1,23 +1,37 @@
 ;; test si grid-copy fonctionne
 
 (defun test-copy-grid(grid)
-  (let((g (grid-copy grid))
-       (e 0))
+  (let((g (grid-copy grid)))
+    (if (equalp g grid)
+	T
+	NIL)))
+;       (e 0))
+;   (loop for i below +SIZE+
+;	  do (loop for j below +SIZE+
+;		  do (if (/=(aref grid i j)(aref g i j))
+;			 (setf e 1))))
+;    (if (eq e 1)
+;	NIL
+;	T)))
+
+;;test si test-valeur fonctionne en lui passant une grid et une grid contenant
+;;toute la liste de possibilité et vérifie qu'elle peut la générer sans erreur
+;;Pour que ce test fonctionne les fonction test-collone test-ligne et test-care doive également etre fonctionnel
+;;On considerera donc que ce test vérifie aussi ces fonctions.
+(defun test-test-valeur (grid vrais-grid)
+  (let ((g (make-array '(9 9):initial-element '(1 2 3 4 5 6 7 8 9))))
     (loop for i below +SIZE+
 	  do (loop for j below +SIZE+
-		  do (if (/=(aref grid i j)(aref g i j))
-			 (setf e 1))))
-    (if (eq e 1)
-	NIL
-	T)))
+		   do (loop for v from 1 to +SIZE+
+			    do (if (not(test-valeur grid j i v))
+				   (setf (aref g i j)(remove v (aref g i j)))))))
+    (equalp g vrais-grid)))
+	
 
-;;test si test-valeur fonctionne en lui passant une grid et une grid contenant 
-;;toute la liste de possibilité et vérifie qu'elle peut la générer sans erreur
 
-(defun initialise-tab-strat (grid vrais-grid)
-  (let ((g (make-array '(9 9):initial-element '(NIL)))
-    (loop for i below 9
-       do (loop for j below 9
-	       do (loop for v from 1 to 9
-		     do (if (test-valeur grid i j v
-			      (setf (aref g i j) ))))))
+
+(defun main-test()
+  (if (and(test-copy-grid +grid-test+)
+	  (test-test-valeur +grid-test+ +possible-grid-test+))
+      T
+      NIL))
